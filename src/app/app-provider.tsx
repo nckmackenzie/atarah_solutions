e1d88c16f7ea077d6b98e33b8e2c37994e676915
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-02 13:38:27 +0300

    feat: define providers for app

--- src/app/app-provider.tsx
+++ src/app/app-provider.tsx
@@ -1,9 +1,10 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import { Toaster } from 'sonner';
+import { ThemeProvider } from '@/app/theme-provider';
 
 const queryClient = new QueryClient();
-
 export default function AppProvider({
   children,
 }: {
@@ -11,8 +12,10 @@ export default function AppProvider({
 }) {
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
-      <Toaster position="top-center" />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        {children}
+        <Toaster position="top-center" />
+      </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
