a0034d04c8ee912bd761bf5190aa5a7c2dfb9692
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-02 16:03:09 +0300

    feat: admin route protection,layout anf admin routes

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -2,11 +2,35 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import ErrorPage from '@/components/ui/error-page';
 import LoginPage from '@/features/auth/pages/login';
+import ProtectedRoute from '@/components/ui/protected-route';
+import AppLayout from '@/components/layout/app-layout';
+import ContentWrapper from '@/components/layout/content-wrapper';
+import { adminRoutes } from '@/features/admin/routes';
 
 export const router = createBrowserRouter([
   {
     path: '/login',
     element: <LoginPage />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: '/',
+    errorElement: <ErrorPage />,
+    element: (
+      <ProtectedRoute>
+        <AppLayout />
+      </ProtectedRoute>
+    ),
+    children: [
+      {
+        index: true,
+        element: (
+          <ContentWrapper>
+            <div>Home</div>
+          </ContentWrapper>
+        ),
+        ...adminRoutes,
+      },
+    ],
   },
 ]);
