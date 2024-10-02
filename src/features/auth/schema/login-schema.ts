56c8ef3cd471c8f31a0d7be67977eed17e0fac0d
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-02 14:10:42 +0300

    refactor: reduce password length and remove max from email

--- src/features/auth/schema/login-schema.ts
+++ src/features/auth/schema/login-schema.ts
@@ -1,14 +1,9 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-  email: z
-    .string()
-    .trim()
-    .email('Invalid email address')
-    .min(1, { message: 'Phone number is required.' })
-    .max(10, { message: 'Invalid phone number' }),
+  email: z.string().trim().email('Invalid email address'),
   password: z
     .string()
     .trim()
-    .min(8, 'Password must be at least 8 characters long'),
+    .min(6, 'Password must be at least 8 characters long'),
 });
