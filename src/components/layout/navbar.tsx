5028568bc9115a9bf2734c2de9c1004af443ebba
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-02 14:27:07 +0300

    refactor: navbar for app

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,109 +1,17 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { LogOut, Moon, Sun } from 'lucide-react';
-import { useNavigate } from 'react-router-dom';
+import { Menu } from 'lucide-react';
 
-import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
-import { Button } from '../ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuPortal,
-  DropdownMenuSeparator,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { toast } from 'sonner';
+import { Button } from '@/components/ui/button';
+import UserNav from '@/components/layout/user-nav';
 
-import { useTheme } from '@/app/theme-provider';
-import { useUser } from '@/features/auth/hooks/use-user';
-import { supabase } from '@/lib/supabase/supabase';
-
-export default function UserNav() {
-  const queryClient = useQueryClient();
-  const { user } = useUser();
-  const { setTheme, theme } = useTheme();
-
-  const navigate = useNavigate();
-  const { isPending, mutate } = useMutation({
-    mutationFn: async () => {
-      const { error } = await supabase.auth.signOut();
-
-      if (error) throw new Error(error.message);
-    },
-    onSuccess: () => {
-      queryClient.removeQueries();
-      navigate('/login');
-    },
-    onError: error => {
-      console.error(`🔥🔥LOGOUT ERROR: ${error.message}`);
-      toast.error('😔Failed to log you out');
-    },
-  });
+export default function Navbar() {
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="relative w-8 h-8 rounded-full">
-          <Avatar className="w-8 h-8">
-            <AvatarImage src="/user.png" alt="Avatar" />
-            <AvatarFallback className="bg-transparent">JD</AvatarFallback>
-          </Avatar>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56" align="end" forceMount>
-        <DropdownMenuLabel className="font-normal">
-          <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">
-              {user?.user_metadata?.fullName || 'Jane Doe'}
-            </p>
-            {user?.email && (
-              <p className="text-xs leading-none text-muted-foreground">
-                {user.email}
-              </p>
-            )}
-          </div>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuSub>
-          <DropdownMenuSubTrigger>
-            {theme === 'dark' ? (
-              <Sun className="w-4 h-4 mr-2" />
-            ) : (
-              <Moon className="w-4 h-4 mr-2" />
-            )}
-            <span>Change Theme</span>
-          </DropdownMenuSubTrigger>
-          <DropdownMenuPortal>
-            <DropdownMenuSubContent>
-              <DropdownMenuItem
-                className="hover:cursor-pointer"
-                onClick={() => setTheme('light')}
-              >
-                <Sun className="w-4 h-4 mr-2" />
-                <span>Light</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem
-                className="hover:cursor-pointer"
-                onClick={() => setTheme('dark')}
-              >
-                <Moon className="w-4 h-4 mr-2" />
-                <span>Dark</span>
-              </DropdownMenuItem>
-            </DropdownMenuSubContent>
-          </DropdownMenuPortal>
-        </DropdownMenuSub>
-        <DropdownMenuItem
-          className="hover:cursor-pointer"
-          onClick={() => mutate()}
-          disabled={isPending}
-        >
-          <LogOut className="w-4 h-4 mr-3 text-muted-foreground" />
-          Sign out
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <header className="flex items-center px-4 border-b h-14 bg-background sm:px-8">
+      <Button variant="ghost" size="icon" className="block md:hidden">
+        <Menu className="text-muted-foreground size-4" />
+      </Button>
+      <div className="ml-auto">
+        <UserNav />
+      </div>
+    </header>
   );
 }
