902a10d40580fcce82724ce9328ae87f67eb75d1
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-02 15:23:21 +0300

    refactor: redirect user from login if they are authenticated

--- src/features/auth/pages/login/index.tsx
+++ src/features/auth/pages/login/index.tsx
@@ -1,5 +1,20 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import LoginForm from '@/features/auth/components/login-form';
+import { useUser } from '@/features/auth/hooks/use-user';
 
 export default function LoginPage() {
+  const { isAuthenticated } = useUser();
+  const navigate = useNavigate();
+
+  useEffect(
+    function () {
+      if (isAuthenticated) {
+        navigate('/', { replace: true });
+      }
+    },
+    [isAuthenticated, navigate]
+  );
   return <LoginForm />;
 }
