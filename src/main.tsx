ac9d671741414774d64ef5849ab1eeffeb65a186
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-02 12:47:42 +0300

    refactor: wrap entire app with app provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-
 import './index.css';
 import { router } from './app/routes';
+import AppProvider from './app/app-provider';
+
+// const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AppProvider>
+      <RouterProvider router={router} />
+    </AppProvider>
   </StrictMode>
 );
