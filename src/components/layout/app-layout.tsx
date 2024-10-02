e2e7d06528d9cc593c3e08b665def10f0f24fb2d
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-02 13:57:42 +0300

    feat: app layout

---
+++ src/components/layout/app-layout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import Sidebar from '@/components/layout/sidebar';
+import Navbar from '@/components/layout/navbar';
+
+export default function AppLayout() {
+  return (
+    <>
+      <Sidebar />
+      <main className="h-screen bg-background md:pl-72">
+        <Navbar />
+        <div className="h-[calc(100%-3.5rem)]">
+          <Outlet />
+        </div>
+      </main>
+    </>
+  );
+}
