85a4615026e619c906d2c52defcda575b6377d17
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-02 13:58:48 +0300

    feat: api service for fetching user

---
+++ src/features/auth/api/index.ts
@@ -0,0 +1,11 @@
+import { supabase } from '@/lib/supabase/supabase';
+
+export async function getCurrentUser() {
+  const { data: session } = await supabase.auth.getSession();
+  if (!session.session) return null;
+
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error) throw new Error(error.message);
+  return data?.user;
+}
