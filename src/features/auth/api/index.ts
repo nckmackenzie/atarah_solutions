8a5a14e8dd11b29a64b12276d42b6a4cecb84baf
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-02 15:22:49 +0300

    feat: api service for login

--- src/features/auth/api/index.ts
+++ src/features/auth/api/index.ts
@@ -1,4 +1,16 @@
 import { supabase } from '@/lib/supabase/supabase';
+import type { LoginFormValues } from '@/features/auth/types/index.types';
+
+export async function login(values: LoginFormValues) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: values.email,
+    password: values.password,
+  });
+
+  if (error) throw new Error(error.message);
+
+  return data;
+}
 
 export async function getCurrentUser() {
   const { data: session } = await supabase.auth.getSession();
