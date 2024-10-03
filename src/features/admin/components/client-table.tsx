57c749d9bc6050b3c7106dacf912526ac1839085
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-03 21:12:00 +0300

    chore: moved to clients folder

--- src/features/admin/components/client-table.tsx
+++
@@ -1,8 +0,0 @@
-import type { Customer } from '@/features/admin/types/customer.types';
-
-interface ClientTableProps {
-  data: Customer[];
-}
-export default function ClientTable({ data }: ClientTableProps) {
-  return <div>ClientTable</div>;
-}
