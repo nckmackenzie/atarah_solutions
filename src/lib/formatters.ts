b2c9813e59df2e99681a51f9e3bc0cf57662e188
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-03 14:36:58 +0300

    chore: app formatters

---
+++ src/lib/formatters.ts
@@ -0,0 +1,46 @@
+import { format } from 'date-fns';
+
+export const flattenErrors = (error: Record<string, string[]>) => {
+  return Object.values(error).flat();
+};
+
+export const titleCase = (str: string) => {
+  return str.toLowerCase().replace(/\b\w/g, s => s.toUpperCase());
+};
+
+export const formatDateLong = (date: Date | string) => {
+  return format(new Date(date), 'PPP');
+};
+
+export const dateFormat = (date: Date | string) => {
+  return format(new Date(date), 'yyyy-MM-dd');
+};
+
+export const formatDateReporting = (date: Date | string) => {
+  return format(new Date(date), 'dd/MM/yyyy');
+};
+
+export const extractValue = (value: string) => {
+  return value.split('-')[0];
+};
+
+export const extractSelectedText = (value: string) => {
+  return value.split('-')[1];
+};
+
+export function numberFormat(
+  number: string | number,
+  minimumFractionDigits = 2
+) {
+  return new Intl.NumberFormat('en-KE', {
+    maximumFractionDigits: 2,
+    minimumFractionDigits,
+  }).format(Number(number));
+}
+
+export const compactNumberFormatter = (value: string | number) => {
+  return new Intl.NumberFormat('en-KE', {
+    notation: 'compact',
+    maximumFractionDigits: 2,
+  }).format(Number(value));
+};
