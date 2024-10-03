ed94a00e2838496c51a7ed526d433e012076f2e0
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-03 16:45:48 +0300

    chore: add various types

--- src/types/index.types.ts
+++ src/types/index.types.ts
@@ -1,3 +1,26 @@
 import { Database } from '@/lib/supabase/database.types';
 
 export type Form = Database['public']['Tables']['forms']['Row'];
+
+export type Option = { value: string; label: string };
+export type ServerError = Record<string, string[]>;
+
+export interface WithId {
+  id: string;
+}
+
+export interface WithName {
+  name: string;
+}
+
+export interface WithIdAndName extends WithId, WithName {}
+
+export interface IsEdit {
+  isEdit?: boolean;
+}
+
+export type IsEditRequired = Required<IsEdit>;
+
+export interface IsPending {
+  isPending: boolean;
+}
