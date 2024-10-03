621362df7ea5ee60e2312488186f4c63a68b8893
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-03 22:04:49 +0300

    feat: handle client fetching and different state.

--- src/features/admin/pages/customers/create-edit.tsx
+++ src/features/admin/pages/customers/create-edit.tsx
@@ -1,22 +1,32 @@
 import PageCard from '@/components/ui/page-card';
-import { useDocumentTitle } from '@/hooks/use-title';
-import type { IsEdit } from '@/types/index.types';
-import ClientForm from '../../components/client-form';
+import ClientForm from '@/features/admin/components/clients/client-form';
 import ContentWrapper from '@/components/layout/content-wrapper';
 import { BackButton } from '@/components/ui/button';
 
+import { useDocumentTitle } from '@/hooks/use-title';
+import { useFetchSingle } from '@/hooks/use-fetch-single';
+import { fetchClient } from '@/features/admin/api/customer';
+import type { IsEdit } from '@/types/index.types';
+import { ErrorAlert } from '@/components/ui/custom-alert';
+import { PageLoader } from '@/components/ui/loaders';
+
 export default function CreateEditForm({ isEdit }: IsEdit) {
   useDocumentTitle(isEdit ? 'Edit client' : 'Create new client');
+  const { data, error, isLoading } = useFetchSingle('customer', fetchClient);
+
+  if (isLoading) return <PageLoader loaderText="Fetcing client details..." />;
+
   return (
     <ContentWrapper>
       <div className="y-spacing">
         <BackButton />
+        {error && <ErrorAlert error={error.message} />}
         <PageCard
           title={isEdit ? 'Edit client' : 'Create new client'}
           hasSearch={false}
           className="max-w-4xl mx-4 md:mx-auto"
         >
-          <ClientForm isEdit={!!isEdit} />
+          <ClientForm isEdit={!!isEdit} data={data} />
         </PageCard>
       </div>
     </ContentWrapper>
