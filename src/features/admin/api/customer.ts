342243e827adcca837a3f6a9ed528dc00697ac77
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-03 22:03:33 +0300

    feat: api methods for update,fetch one and delete client

--- src/features/admin/api/customer.ts
+++ src/features/admin/api/customer.ts
@@ -1,4 +1,5 @@
 import type { CustomerFormValues } from '@/features/admin/types/customer.types';
+import { dateFormat } from '@/lib/formatters';
 import { supabase } from '@/lib/supabase/supabase';
 
 export async function fetchClients(query?: string) {
@@ -7,7 +8,7 @@ export async function fetchClients(query?: string) {
     qry = qry.or(`name.like.%${query}%,contact.like.%${query}%`);
   }
 
-  const { data, error } = await qry;
+  const { data, error } = await qry.order('name', { ascending: true });
 
   if (error) throw new Error(error.message);
 
@@ -15,7 +16,7 @@ export async function fetchClients(query?: string) {
 }
 
 export async function createClient(values: CustomerFormValues) {
-  const { error } = await supabase
+  const { data, error } = await supabase
     .from('clients')
     .insert([
       {
@@ -25,5 +26,54 @@ export async function createClient(values: CustomerFormValues) {
     ])
     .select();
 
+  if (error) throw new Error(error.message);
+
+  if (values?.openingBalance > 0) {
+    const { error: insertError } = await supabase
+      .from('invoice_payments')
+      .insert([
+        {
+          transactionDate: dateFormat(values.openingBalanceDate!),
+          amount: values.openingBalance,
+          transacfionType: 'opening_balance',
+        },
+      ])
+      .select();
+
+    if (insertError) {
+      await supabase.from('clients').delete().eq('id', data[0].id);
+      throw new Error(insertError.message);
+    }
+  }
+}
+
+export async function fetchClient(id: string) {
+  const { data, error } = await supabase
+    .from('clients')
+    .select('*')
+    .eq('id', id)
+    .single();
+  if (error) throw new Error(error.message);
+  return data;
+}
+
+export async function updateClient(id: string, values: CustomerFormValues) {
+  const { error } = await supabase
+    .from('clients')
+    .update({
+      active: values.active,
+      address: values.address,
+      contact: values.contact,
+      email: values.email,
+      name: values.name,
+      taxPin: values.taxPin,
+    })
+    .eq('id', id);
+
+  if (error) throw new Error(error.message);
+}
+
+export async function deleteClient(id: string) {
+  const { error } = await supabase.from('clients').delete().eq('id', id);
   if (error) throw new Error(error.message);
 }
