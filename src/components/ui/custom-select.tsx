683a1dfae735bfe80a8b66617db6a389322df9ca
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-05 16:22:32 +0300

    feat: reusable component

---
+++ src/components/ui/custom-select.tsx
@@ -0,0 +1,40 @@
+import { SearchSelect, SearchSelectItem } from '@tremor/react';
+import { type Option } from '@/types/index.types';
+
+interface CustomSearchSelectProps {
+  options: Option[];
+  className?: string;
+  defaultValue?: string;
+  disabled?: boolean;
+  value?: string;
+  onChange?: (value: string) => void;
+  placeholder?: string;
+  enableClear?: boolean;
+}
+
+export default function CustomSearchSelect({
+  options,
+  defaultValue,
+  onChange,
+  disabled,
+  value,
+  placeholder,
+  enableClear = true,
+}: CustomSearchSelectProps) {
+  return (
+    <SearchSelect
+      disabled={disabled}
+      onValueChange={onChange}
+      enableClear={enableClear}
+      defaultValue={defaultValue}
+      value={value}
+      placeholder={placeholder || 'Select one...'}
+    >
+      {options.map(option => (
+        <SearchSelectItem key={option.value} value={option.value}>
+          {option.label}
+        </SearchSelectItem>
+      ))}
+    </SearchSelect>
+  );
+}
