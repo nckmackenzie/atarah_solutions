c9daf20c5a9725484d01c107a4133829fd005b33
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-06 19:31:12 +0300

    chore: add types for invoices

--- src/features/transactions/types/invoice.types.ts
+++ src/features/transactions/types/invoice.types.ts
@@ -1,11 +1,24 @@
 import { z } from 'zod';
-import { invoiceFormSchema } from '@/features/transactions/schema/invoice';
+import {
+  invoiceFormSchema,
+  invoicePaymentFormSchema,
+} from '@/features/transactions/schema/invoice';
 import {
   fetchInvoices,
   fetchInvoice,
+  fetchInvoicePayments,
+  fetchInvoicePayment,
 } from '@/features/transactions/api/invoice';
+import { Database } from '@/lib/supabase/database.types';
 
 export type InvoiceFormValues = z.infer<typeof invoiceFormSchema>;
 export type VatType = InvoiceFormValues['vatType'];
 export type InvoiceRow = Awaited<ReturnType<typeof fetchInvoices>>[number];
 export type Invoice = Awaited<ReturnType<typeof fetchInvoice>>;
+export type InvoicePaymentFormValues = z.infer<typeof invoicePaymentFormSchema>;
+export type PaymentType = Database['public']['Enums']['invoice_payment_type'];
+export type InvoicePaymentRow = Awaited<
+  ReturnType<typeof fetchInvoicePayments>
+>[number];
+
+export type InvoicePayment = Awaited<ReturnType<typeof fetchInvoicePayment>>;
