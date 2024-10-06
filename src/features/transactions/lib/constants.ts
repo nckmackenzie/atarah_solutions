0aecbe197e3edcb1c38c7e95cc1fd16b9a9d8f85
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-06 08:52:39 +0300

    chore: constants for vat and terms

---
+++ src/features/transactions/lib/constants.ts
@@ -0,0 +1,11 @@
+export const TERMS = [
+  { value: '0', label: 'Due on Receipt' },
+  { value: '1', label: '30 Days' },
+  { value: '2', label: '60 Days' },
+];
+
+export const VAT_TYPES = [
+  { value: 'no_vat', label: 'No VAT' },
+  { value: 'inclusive', label: 'Inclusive VAT' },
+  { value: 'exclusive', label: 'Exclusive VAT' },
+];
