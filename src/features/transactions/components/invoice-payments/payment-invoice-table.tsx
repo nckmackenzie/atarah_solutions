25bb2d04af092fabb071448097be3c9134a9844c
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-06 18:00:24 +0300

    feat: table for pending invoices

---
+++ src/features/transactions/components/invoice-payments/payment-invoice-table.tsx
@@ -0,0 +1,62 @@
+import { format } from 'date-fns';
+import { useFieldArray, type UseFormReturn } from 'react-hook-form';
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { Input } from '@/components/ui/input';
+
+import { numberFormat } from '@/lib/formatters';
+import type { InvoicePaymentFormValues } from '@/features/transactions/types/invoice.types';
+import type { IsPending } from '@/types/index.types';
+interface PaymentInvoiceTableProps extends IsPending {
+  form: UseFormReturn<InvoicePaymentFormValues>;
+}
+
+export default function PaymentInvoiceTable({
+  form,
+  isPending,
+}: PaymentInvoiceTableProps) {
+  const { fields } = useFieldArray({
+    control: form.control,
+    name: 'invoices',
+  });
+
+  return (
+    <div className="col-span-full">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Invoice</TableHead>
+            <TableHead>Due Date</TableHead>
+            <TableHead>Invoice Amount</TableHead>
+            <TableHead>Balance</TableHead>
+            <TableHead className="w-56">Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {fields.map((field, index) => (
+            <TableRow key={field.invoiceId}>
+              <TableCell>{field.invoiceNo}</TableCell>
+              <TableCell>{format(field.dueDate, 'dd/MMM/yyyy')}</TableCell>
+              <TableCell>{numberFormat(field.invoiceAmount)}</TableCell>
+              <TableCell>{numberFormat(field.balance)}</TableCell>
+              <TableCell>
+                <Input
+                  className="w-56"
+                  disabled={isPending}
+                  {...form.register(`invoices.${index}.amountPaid`)}
+                />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
