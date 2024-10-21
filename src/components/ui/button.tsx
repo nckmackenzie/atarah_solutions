398921e14cfe5127e424ba1126348aa25f77f342
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-21 09:45:55 +0300

    style: add variant for excel

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,6 +19,8 @@ const buttonVariants = cva(
           'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
         secondary:
           'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
+        excel:
+          'bg-green-300 dark:bg-green-700 text-green-800 dark:text-green-100 shadow-sm hover:bg-green-400 hover:dark:bg-green-800',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
