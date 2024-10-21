96e32c2881cdd3053f260caf6523097e95245d1c
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-21 17:35:55 +0300

    chore: schemas for different report forms

---
+++ src/features/reports/schema/index.ts
@@ -0,0 +1,49 @@
+import { z } from 'zod';
+
+export const paymentReportSchema = z
+  .object({
+    reportType: z.enum(['all', 'by-client'], {
+      required_error: 'Report type is required',
+    }),
+    clientId: z.string().optional(),
+  })
+  .superRefine(({ reportType, clientId }, ctx) => {
+    if (reportType === 'by-client' && !clientId) {
+      ctx.addIssue({
+        code: 'custom',
+        path: ['clientId'],
+        message: 'Client is required',
+      });
+    }
+  });
+
+export const expensesReportSchema = z
+  .object({
+    reportType: z.enum(['all', 'by-account', 'by-project'], {
+      required_error: 'Report type is required',
+    }),
+    // projectId: z.string().optional(),
+    accountId: z.coerce.number().optional(),
+  })
+  .superRefine(({ reportType, accountId }, ctx) => {
+    // if (reportType !== 'by-project' && !projectId) {
+    //   ctx.addIssue({
+    //     code: 'custom',
+    //     path: ['projectId'],
+    //     message: 'Project is required',
+    //   });
+    // }
+    if (reportType === 'by-account' && !accountId) {
+      ctx.addIssue({
+        code: 'custom',
+        path: ['accountId'],
+        message: 'Account is required',
+      });
+    }
+  });
+
+export const clientStatementReportSchema = z.object({
+  clientId: z
+    .string({ required_error: 'Client is required' })
+    .min(1, 'Client is required'),
+});
